8d9020ca773b1a7254713ea2d59d6ce9bf840b7f
Swilam20 <38882447+Swilam20@users.noreply.github.com>
2020-06-19 16:56:14 +0200

    Create  STD_Types.h

---
+++ STD_Types.h
@@ -0,0 +1,27 @@
+#ifndef STD_TYPES
+#define STD_TYPES
+
+
+/* Importnant Macros */
+#define       ENABLE         1
+#define       DISABLE        0
+#define       TRUE           1
+#define       FALSE          1
+
+/* Unsigned */
+typedef   unsigned char              uint8_t;
+typedef   unsigned short             uint16_t;
+typedef   unsigned int	             uint32_t;
+typedef   unsigned long long int     uint64_t;
+
+/* Signed */
+typedef   signed char                int8_t;
+typedef   short                      int16_t;
+typedef   int                        int32_t;
+typedef   long long int              int64_t;
+typedef   float                      float32_t;
+typedef   double                     float64_t;
+
+
+
+#endif
