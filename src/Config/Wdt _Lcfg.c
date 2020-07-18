6f25dba7f2885858a8899b04f40d45f317a28d59
Swilam20 <38882447+Swilam20@users.noreply.github.com>
2020-07-18 17:21:27 +0200

    Add files via upload

---
+++ src/Config/Wdt _Lcfg.c
@@ -0,0 +1,9 @@
+#include "Wdt.h"
+
+
+const Wdt_ConfigType Wdt_Cfg
+
+{	/*WdgInitialTimeout						dgMaxTimeoutWDT DRIVER 				interruptType(STANDARD\NON_MASKABLE*/
+
+		100											1000										STANDARD
+}
